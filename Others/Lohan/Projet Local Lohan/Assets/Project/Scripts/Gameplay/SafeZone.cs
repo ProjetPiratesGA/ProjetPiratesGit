a004cfe72d3eb0072af543f02154112cf8798ae3
LohanBlanc <projetpiratesga@gmail.com>
2020-01-03 15:57:52 +0100

    projet local lohan cooldown repair, safe zone transparency

--- Others/Lohan/Projet Local Lohan/Assets/Project/Scripts/Gameplay/SafeZone.cs
+++ Others/Lohan/Projet Local Lohan/Assets/Project/Scripts/Gameplay/SafeZone.cs
@@ -17,10 +17,26 @@ public class SafeZone : MonoBehaviour {
     private void OnTriggerEnter(Collider other)
     {
         //Check if object in collision is a player
-        if (other.GetComponent<ProjetPirate.Boat.BoatCharacter>() != null)
+        if (other.GetComponent<ProjetPirate.Boat.BoatCharacter>() != null && other.GetComponent<ProjetPirate.Boat.BoatCharacter>().Safe == false)
         {
             //Put all the associated enemies on alert if they aren't already
             other.GetComponent<ProjetPirate.Boat.BoatCharacter>().Safe = true;
+            MeshRenderer[] children = other.GetComponentsInChildren<MeshRenderer>();
+            Color newColor;
+            foreach (MeshRenderer child in children)
+            {
+                newColor = child.material.color;
+                newColor.a = 0.7f;
+                child.material.color = newColor;
+                child.material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                child.material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                child.material.SetInt("_ZWrite", 0);
+                child.material.DisableKeyword("_ALPHATEST_ON");
+                child.material.DisableKeyword("_ALPHABLEND_ON");
+                child.material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+                child.material.renderQueue = 3000;
+            }
+
         }
     }
 
@@ -31,6 +47,22 @@ public class SafeZone : MonoBehaviour {
         {
             //Put all the associated enemies on alert if they aren't already
             other.GetComponent<ProjetPirate.Boat.BoatCharacter>().Safe = false;
+
+            MeshRenderer[] children = other.GetComponentsInChildren<MeshRenderer>();
+            Color newColor;
+            foreach (MeshRenderer child in children)
+            {
+                newColor = child.material.color;
+                newColor.a = 0.7f;
+                child.material.color = newColor;
+                child.material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                child.material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                child.material.SetInt("_ZWrite", 1);
+                child.material.DisableKeyword("_ALPHATEST_ON");
+                child.material.DisableKeyword("_ALPHABLEND_ON");
+                child.material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                child.material.renderQueue = -1;
+            }
         }
     }
 }
