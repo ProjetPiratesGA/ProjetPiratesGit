a1779dde4f10934cacb95cbe201904972528f009
PeZeD <pezed30@gmail.com>
2020-01-06 16:58:22 +0100

    Push save Datas

---
+++ Base Sink'em All Copie Data/Assets/Project/Scripts/Datas/Data_Amelioration.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ProjetPirate.Data
+{
+    [System.Serializable]
+    public class Data_Amelioration
+    {
+        private int item = -1;
+        private int level = 2;
+        private int price = -1;
+        private float reputationNecessary = -1;
+
+        public int Item { get { return item; } set { item = value; } }
+        public int Price { get { return price; } set { price = value; } }
+        public int Level { get { return level; } set { level = value; } }
+        public float ReputationNecessary { get { return reputationNecessary; } set { reputationNecessary = value; } }
+    }
+}
---
+++ Base Sink'em All Copie Data/Assets/Project/Scripts/Datas/Data_bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjetPirate.Data
+{
+    [System.Serializable]
+    public class Data_bullet
+    {
+        public Data_bullet(int _damage, int _id )
+        {
+            damage = _damage;
+            id = _id;
+        }
+
+        private int damage = 1;
+        private int id = 1;
+
+        public  int Damage { get { return damage; } set { damage = value; } }
+        /* get the id of the bulklet, it is supposed to have the same as the canon that shot it */
+        public  int ID { get { return id; } set { id = value; } }
+
+    }
+
+}
