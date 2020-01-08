cc1d4b2a87a3426371fa49aab40012f8386c4017
TailladeGuillaume <58856520+TailladeGuillaume@users.noreply.github.com>
2020-01-08 16:53:12 +0100

    modif guillaume

---
+++ Others/Guillaume/Base Sink'em All copie/Assets/Project/Scripts/Datas/YanisTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ProjetPirate.Data;
+public class YanisTest : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    void Awake()
+    {
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
