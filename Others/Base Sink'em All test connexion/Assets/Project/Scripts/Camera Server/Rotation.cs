b796b2e5ee16fbeb41318008378033d8eb9a902d
Seb <pezed30@gmail.com>
2020-01-09 00:01:23 +0100

    Cam serveur

---
+++ Others/Base Sink'em All test connexion/Assets/Project/Scripts/Camera Server/Rotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+    [SerializeField]
+    float _speed = 120f;
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        this.transform.Rotate(Vector3.up, horizontal * Time.deltaTime * _speed);
+
+    }
+}
