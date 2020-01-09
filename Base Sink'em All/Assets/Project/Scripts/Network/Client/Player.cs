b0fb17ce8042b244dd531f19e14c1a52475a0eda
PeZeD <pezed30@gmail.com>
2020-01-09 16:08:35 +0100

    Changement de level bateau via F11

--- Base Sink'em All/Assets/Project/Scripts/Network/Client/Player.cs
+++ Base Sink'em All/Assets/Project/Scripts/Network/Client/Player.cs
@@ -181,6 +181,17 @@ public class Player : Controller
                 _debugLogDisplayTimer = 0;
                 CmdSendDebug(_data.Ressource.Golds);
             }
+
+            if(Input.GetKeyDown(KeyCode.F11))
+            {
+                if(this._data.Ressource.BoatLevel < 3)
+                {
+                    this._data.Ressource.BoatLevel += 1;
+                    this.CmdSendBoatLevel(this._data.Ressource.BoatLevel);
+                    this.CmdSpawnBoat();
+                }
+            }
+
         }
         ///FIN DEBUG
         CheckQuest();
@@ -255,6 +266,20 @@ public class Player : Controller
     //{
     //    RpcSendMaxCanonPerSide(this._data.Boat.MaxCanonPerSide);
     //}
+    [ClientRpc]
+    public void RpcSendBoatLevel(int level)
+    {
+        this._data.Ressource.BoatLevel = level;
+    }
+
+
+    [Command]
+    public void CmdSendBoatLevel(int level)
+    {
+        this._data.Ressource.BoatLevel = level;
+    }
+
+
     [Command]
     public void CmdSendCurrentCanonLeft(int number)
     {
@@ -621,9 +646,10 @@ public class Player : Controller
     }
 
     [ClientRpc]
-    public void RpcSetStartData(int life, int gold, int cannonLeft, int cannonRight, float speed, Vector3 position, Vector4 rotation)
+    public void RpcSetStartData(int life, int gold, int cannonLeft, int cannonRight, float speed, Vector3 position, Vector4 rotation, int level)
     {
         this.data.Ressource.Golds = gold;
+        this.data.Ressource.BoatLevel = level;
         this.data.Boat.Stats.Life = life;
         this.data.Boat.CurrentCanonLeft = cannonLeft;
         this.data.Boat.CurrentCanonRight = cannonRight;    
@@ -645,9 +671,10 @@ public class Player : Controller
 
     }
     [TargetRpc]
-    public void TargetSetStartData(NetworkConnection target, int life, int gold, int cannonLeft, int cannonRight, float speed, Vector3 position, Vector4 rotation)
+    public void TargetSetStartData(NetworkConnection target, int life, int gold, int cannonLeft, int cannonRight, float speed, Vector3 position, Vector4 rotation, int level)
     {
         this.data.Ressource.Golds = gold;
+        this.data.Ressource.BoatLevel = level;
         this.data.Boat.Stats.Life = life;
         this.data.Boat.CurrentCanonLeft = cannonLeft;
         this.data.Boat.CurrentCanonRight = cannonRight;      
